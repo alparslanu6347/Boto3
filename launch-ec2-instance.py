e0aec8a9062726fd1febcf10a06209c4a13867ee
alparslanu6347 <alparslanu6347@gmail.com>
2024-03-31 18:30:45 +0300

    files updated

--- launch-ec2-instance.py
+++ launch-ec2-instance.py
@@ -5,11 +5,11 @@ ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
 instance = ec2.create_instances(
-     ImageId='ami-0aa7d40eeae50c9a9',
+     ImageId='ami-0aa7d40eeae50c9a9',   # DO NOT FORGET TO CHANGE ME
      MinCount=1,
      MaxCount=1,
-     InstanceType='t2.micro',
-     KeyName='second-key'
+     InstanceType='t2.micro',           # 
+     KeyName='second-key'               # DO NOT FORGET TO CHANGE ME
  )
 
 print(instance)
