82211a80fbfa6939d30e9bebc5e7e8ef232d2ac3
alparslanu6347 <alparslanu6347@gmail.com>
2024-03-31 18:31:17 +0300

    files updated

--- launch-ec2.py
+++ launch-ec2.py
@@ -1,7 +1,7 @@
 import boto3
 
 client = boto3.client("ec2")
-resp = client.run_instances(ImageId="ami-0aa7d40eeae50c9a9",
+resp = client.run_instances(ImageId="ami-0aa7d40eeae50c9a9",    #  DO NOT FORGET TO CHANGE ME
                     InstanceType="t2.micro",
                     MinCount=1,
                     MaxCount=1)
